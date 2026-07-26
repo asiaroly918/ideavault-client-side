840d8d57e19a52c81689c50fdb64ba2d8560d25a
asiaroly <a.k.rolybd@gmail.com>
2026-07-27 01:28:48 +0600

    fix api baseurl and env config

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { dummyIdeas, dummyComments, demoUser } from './dummyData';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || '',
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL || ''}/api`,
   headers: { 'Content-Type': 'application/json' },
 });
 
